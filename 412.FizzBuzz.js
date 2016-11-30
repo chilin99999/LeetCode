b8b890782a8f476e7afb6b8fe6a4603e228d7495
chilin99999 <chilin99999@gmail.com>
2016-11-30 23:38:11 +0800

    Create 412.FizzBuzz.js

---
+++ 412.FizzBuzz.js
@@ -0,0 +1,27 @@
+// #413 Fizz Buzz - Easy
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    var output = [];
+    var temp,flag;
+    
+    for(var i=1; i<=n; i++) {
+        temp = "", flag = false;
+        if(i%3 === 0) {
+            temp += "Fizz";
+            flag = true;
+        }
+        if(i%5 === 0)
+        {
+            temp += "Buzz";
+            flag = true;
+        }
+        if(flag === false)
+            temp += i;
+        output.push(temp);
+    }
+    return output;
+};
